759a930a8f90e029b579c6218f557c0f634fea60
ZerreMC <69958471+zerremc@users.noreply.github.com>
2020-08-22 11:08:26 +0200

    Mucho tiempo pa poca cosa. Solo diseño del main activity_main.xml

--- app/src/main/java/com/zerre/guitarapp/MainActivity.kt
+++ app/src/main/java/com/zerre/guitarapp/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.zerre.guitarapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Drawer Layout
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.miItem1 -> Toast.makeText(applicationContext,
+                    "Item 1 seleccionado", Toast.LENGTH_SHORT).show()
+                R.id.miItem2 -> Toast.makeText(applicationContext,
+                    "Item 2seleccionado", Toast.LENGTH_SHORT).show()
+                R.id.miItem3 -> Toast.makeText(applicationContext,
+                    "Item 3 seleccionado", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }//Final del Drawer Layout
 }
